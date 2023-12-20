5cfa84d9ac1d01e75ceba1d80498c801798b1f5e
luiggy28 <luiggymusica@gmail.com>
2023-12-20 07:56:49 -0300

    actualizacion

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,73 +1,82 @@
 export const MOCK_DATA = [
     { 
-        id: 1, name: "Micrófono AKG C414",
+        id: 1,
+        name: "Micrófono AKG C414",
         img: "https://dummyimage.com/250x250/000/fff",
         price: 1200,
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',
+        description: 'Un micrófono de condensador legendario, conocido por su versatilidad y cinco patrones polares seleccionables. Ideal para grabaciones de estudio detalladas, voces, y una amplia gama de instrumentos.',
         categoria: "Categoría E"
-    },
-    { 
-        id: 2,
-        name: "Micrófono Neumann U 87",
-        img: "https://dummyimage.com/250x250/000/fff", 
-        price: 3200,
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',
-        categoria: "Categoría E" 
-    },
-    { 
-        id: 3, 
-        name: "Micrófono Universal Audio Bock 167", 
-        img: "https://dummyimage.com/250x250/000/fff",  
-        price: 3400, 
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',  
-        categoria: "Categoría E" 
-    },
-    { 
-        id: 4, 
-        name: "Monitor Estudio Focal Trio6 St6", 
-        img: "https://dummyimage.com/250x250/000/fff",  
-        price: 3500, 
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',  
-        categoria: "Categoría M" 
-    },
-    { 
-        id: 5, 
-        name: "Monitor Estudio Neumann KH 150", 
-        img: "https://dummyimage.com/250x250/000/fff",  
-        price: 2000, 
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!', 
-        categoria: "Categoría M" 
-    },
-    { 
-        id: 6, 
-        name: "Monitor Estudio Yamaha HS7W", 
-        img: "https://dummyimage.com/250x250/000/fff",  
-        price: 460, 
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',  
-        categoria: "Categoría M" 
-    },
-    { 
-        id: 7, 
-        name: "Controlador M-Audio Hammer 88 / USB 88 teclas", 
-        img: "https://dummyimage.com/250x250/000/fff",  
-        price: 500, 
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',  
-        categoria: "Categoría C" 
     },        
     { 
-        id: 8, 
-        name: "Controlador Arturia KEYLABESS61 MK3 USB/MIDI 61 Teclas Essentials", 
-        img: "https://dummyimage.com/250x250/000/fff",  
-        price: 300, 
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',  
-        categoria: "Categoría C" 
-    },       
+        id: 2,
+        name: "Micrófono Shure SM7B",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 800,
+        description: 'Este micrófono dinámico es famoso por su uso en grabaciones vocales de radio y podcasting. Ofrece un sonido cálido y suave con excelente aislamiento de ruidos de fondo.',
+        categoria: "Categoría E"
+    },        
     { 
-        id: 9, 
-        name: "Controlador M-Audio Oxygen 49 MKV 49 Teclas", 
-        img: "https://dummyimage.com/250x250/000/fff",  
-        price: 130, 
-        description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam, error libero quaerat eos molestiae, nisi in excepturi facere velit omnis sequi laudantium ipsum eaque. Soluta molestiae inventore illo sequi deleniti!!',  
-        categoria: "Categoría C" 
-    }        
+        id: 3,
+        name: "Micrófono Rode NT1-A",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 980,
+        description: 'Un micrófono de condensador con una de las menores cifras de ruido autoimpuesto en su clase. Es popular para grabaciones de voz en casa y estudio por su sonido claro y cálido.',
+        categoria: "Categoría E"
+    },        
+    { 
+        id: 4,
+        name: "Micrófono Neumann U 87",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 1780,
+        description: 'Este clásico de estudio ofrece un sonido cálido y bien balanceado. Es altamente versátil y utilizado por profesionales para grabaciones de voces, instrumentos y más.',
+        categoria: "Categoría E"
+    },        
+    { 
+        id: 5,
+        name: "Micrófono Blue Yeti USB",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 680,
+        description: 'Un micrófono USB multipatrón muy popular entre streamers y podcasters. Ofrece una excelente calidad de sonido y es muy fácil de usar para grabaciones en casa.',
+        categoria: "Categoría E"
+    },        
+    { 
+        id: 6,
+        name: "Micrófono Sennheiser MD 421",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 795,
+        description: 'Conocido por su robustez y versatilidad, este micrófono dinámico es excelente para grabar guitarras, baterías y voces, tanto en estudio como en vivo.',
+        categoria: "Categoría E"
+    },        
+    { 
+        id: 7,
+        name: "Micrófono Electro-Voice RE20",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 1350,
+        description: 'Un micrófono dinámico ideal para aplicaciones de broadcast y grabación. Ofrece un sonido consistente y controlado, muy usado para voces habladas y radio.',
+        categoria: "Categoría E"
+    },        
+    { 
+        id: 8,
+        name: "Micrófono Audio-Technica AT2020",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 250,
+        description: 'Un micrófono de condensador de diafragma grande, ideal para proyectos y estudios caseros. Ofrece un sonido detallado y es adecuado para una variedad de aplicaciones.',
+        categoria: "Categoría E"
+    },        
+    { 
+        id: 9,
+        name: "Micrófono Royer R-121 Ribbon",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 560,
+        description: 'Este micrófono de cinta es famoso por su sonido natural y su capacidad para manejar altos SPLs. Es excelente para capturar la esencia de instrumentos como guitarras y metales.',
+        categoria: "Categoría E"
+    },        
+    { 
+        id: 9,
+        name: "Micrófono Behringer C-1",
+        img: "https://dummyimage.com/250x250/000/fff",
+        price: 560,
+        description: 'Un micrófono de condensador de estudio asequible y de alta calidad. Ideal para capturar voces e instrumentos con claridad y detalle.',
+        categoria: "Categoría E"
+    },        
 ];
